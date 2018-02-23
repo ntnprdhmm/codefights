a6bc318e18bb91beb1c1cca0dc9dae74ad1e1c33
prudywsh <antoineprudhomme5@gmail.com>
2018-02-23 19:45:55 +0100

    the journey begins

---
+++ arcade/the_journey_begins/centuryFromYear.java
@@ -0,0 +1,7 @@
+int centuryFromYear(int year) {
+    int century = (int)(year / 100);
+    if (year % 100 != 0) {
+        century += 1;
+    }
+    return century;
+}
---
+++ arcade/the_journey_begins/checkPalindrome.java
@@ -0,0 +1,8 @@
+boolean checkPalindrome(String s) {
+    for (int i = 0; i < Math.floor(s.length() / 2); i++) {
+        if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
